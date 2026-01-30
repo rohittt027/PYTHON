dc41e0f27288ec28f516a79d141418b0dcd761ad
rohittt027 <rohitkaithwaria99gmail@gmail.com>
2026-01-30 22:51:34 +0530

    update this file

---
+++ Operators/Arithmetic.py
@@ -0,0 +1,26 @@
+# x = 10
+# y = 3
+
+# print("The output of x + y : ", x + y) 
+# print("The output of x - y : ", x - y)
+# print("The output of x * y : ", x * y)
+# print("The output of x / y : ", x / y)
+# print("The output of x % y : ", x % y)
+# print("The output of x // y : ", x // y)
+# print("The output of x ** y : ", x ** y)
+
+
+
+
+# x = int(input("Enter value of x: "))
+# y = int(input("Enter value of y: "))
+
+# print("The output of x + y : ", x + y)
+# print("The output of x - y : ", x - y)
+# print("The output of x * y : ", x * y)
+# print("The output of x / y : ", x / y)
+# print("The output of x % y : ", x % y)
+# print("The output of x // y : ", x // y)
+# print("The output of x ** y : ", x ** y)
+
+
--- hello.py
+++ hello.py
@@ -1,6 +1,12 @@
-print("===================================")
-print("Hello World!")
-print("My Name is Rohit Kaithwaria")
-print("Welcome in 2026")
-print("A New Year, A New Beginning")
-print("===================================")
+# print("Hello World!")
+# print("My Name is Rohit Kaithwaria")
+# print("Welcome in 2026")
+
+
+
+# first_name = input("Enter your first name: ")
+# last_name = input("Enter your last name: ")
+
+# print("Your name is:", first_name, last_name)
+
+
