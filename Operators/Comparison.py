d3676712c8dad1fe1dd479479f76dfb798a28249
rohittt027 <rohitkaithwaria99gmail@gmail.com>
2026-01-31 15:14:33 +0530

    commit in github

---
+++ Operators/Comparison.py
@@ -0,0 +1,27 @@
+#3. # COMPARISON OPERATORS :- USED TO COMPARE TWO VALUES AND ALWAYS RETURN A BOOLEAN VALUE (TRUE / FALSE).
+# EX: ==, !=, >, <, >=, <=
+
+first_number = 10
+second_number = 20
+
+print("output:", first_number == second_number)      # False
+print("output:", first_number != second_number)      # True
+print("output:", first_number > second_number)       # False
+print("output:", first_number < second_number)       # True
+print("output:", first_number >= second_number)      # False
+print("output:", first_number <= second_number)      # True
+
+
+
+# TAKES TWO NUMBERS AS INPUT AND COMPARES THEM USING ALL COMPARISON OPERATORS
+
+first_number = int(input("Enter first number: "))
+second_number = int(input("Enter second number: "))
+
+
+print("output:", first_number == second_number)      # True / False
+print("output:", first_number != second_number)      # True / False
+print("output:", first_number > second_number)       # True / False
+print("output:", first_number < second_number)       # True / False
+print("output:", first_number >= second_number)      # True / False
+print("output:", first_number <= second_number)      # True / False
