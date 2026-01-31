cda2284028f6e04ed6826618d0cc11a0ff05cd78
rohittt027 <rohitkaithwaria99gmail@gmail.com>
2026-01-31 10:38:18 +0530

    update

--- Operators/Arithmetic.py
+++ Operators/Arithmetic.py
@@ -1,3 +1,7 @@
+#1. ARITHMETIC OPERATORS => USED TO PERFORM MATHEMATICAL OPERATIONS BETWEEN TWO OPERANDS.
+# EX: +, -, *, %, /, //, **
+# EXAMPLE:
+
 # x = 10
 # y = 3
 
@@ -10,6 +14,7 @@
 # print("The output of x ** y : ", x ** y)
 
 
+# TAKES TWO NUMBERS AS INPUT AND PERFORMS ALL ARITHMETIC OPERATIONS .
 
 # x = int(input("Enter value of x: "))
 # y = int(input("Enter value of y: "))
---
+++ Operators/Assignment.py
@@ -0,0 +1,22 @@
+#2. ASSIGNMENT OPERATORS => USED TO ASSIGN VALUES TO A VARIABLE.
+# EX: +=, -=, *=, %= 
+# EXAMPLE:
+
+x = 15
+y = 5
+
+x+=y
+x-=y
+x/=y
+x%=y
+
+
+print("The value of x is : ",x+y)
+print("The value of x is : ",x-y)
+print("The value of x is : ",x/y)
+print("The value of x is : ",x%y)
+print("The value of x is : ",x**y)
+
+
+
+
