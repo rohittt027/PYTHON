d7f46bde5629f0aa8050fc5a393f57c29b855896
rohittt027 <rohitkaithwaria99@gmail.com>
2026-04-07 22:11:08 +0530

    strings & conditional statements

---
+++ Chapter-02/Conditional Statements/else_statement.py
@@ -0,0 +1,23 @@
+# light = "pink"
+
+# if(light == "red"):
+#     print("stop")
+# elif(light == "green"):
+#     print("go")
+# elif(light == "yellow"):
+#     print("look")
+# else:
+#     print("light is broken")
+    
+
+#     print("end of code")
+
+    # -----------------------------------------------------------------------------------------------
+
+age = 24
+
+if(age >= 18):
+    print("can vote")
+
+else:
+    print("CANNOT vote")
