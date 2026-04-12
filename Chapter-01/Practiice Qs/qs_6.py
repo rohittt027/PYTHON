702db46782aa829bec3f310c5e6a46bd5db9d34b
rohittt027 <rohitkaithwaria99@gmail.com>
2026-04-12 23:26:50 +0530

    practice qs

---
+++ Chapter-01/Practiice Qs/qs_6.py
@@ -0,0 +1,5 @@
+# Take birth year and calculate age.
+birth_year = int(input("enter birth year :"))
+current_year = 2026
+
+print("Age =", (current_year - birth_year) )
---
+++ Chapter-01/Practiice Qs/qs_7.py
@@ -0,0 +1,5 @@
+# Take salary input, add 10% bonus, and print final salary.
+sal = int(input("enter salary :"))
+bonus = int(input("enter bonus :"))
+
+print("final_sal =", (sal * bonus / 100)+sal )
