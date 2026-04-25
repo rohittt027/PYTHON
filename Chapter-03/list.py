a58a270e690bb1c3699916db62dcb97571c08b1d
rohittt027 <rohitkaithwaria99@gmail.com>
2026-04-25 22:10:47 +0530

    list & tuples

---
+++ Chapter-03/list.py
@@ -0,0 +1,30 @@
+# marks1 = 94.4
+# marks2 = 87.5
+# marks = 95.2
+# marks = 45.1
+
+# marks = [94.4, 87.5, 95.2, 45.1]
+# print(marks)
+# print(type(marks))  #list
+# print(marks[0])  #94.4
+# print(marks[1])  #87.5
+# print(marks[3])  #45.1
+# print(len(marks))  #4
+
+
+
+# lst = [88, 66, 77, 99, 100]
+# print(lst)
+# print(type(lst))  #list
+# print(len(lst))  #5
+# print(lst[0])  #88
+# print(lst[4])  #100
+# print(lst[2])  #77
+
+
+
+# student = ["karan", 95.4, 17, "Delhi"]
+# print(student)
+# print(student[0])
+# student[0] = "arjun"
+# print(student)  #['arjun', 95.4, 17, 'Delhi']
---
+++ Chapter-03/negative_lst_slicing.py
@@ -0,0 +1,6 @@
+marks = [87, 64, 33, 95, 76]
+
+print(marks[-1])  #76
+print(marks[-5])  #87
+print(marks[-3:-1])  #[33, 95]
+  
