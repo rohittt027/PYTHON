02266df98863d0cd5211b0a0b5e6593ebd1106e3
rohittt027 <rohitkaithwaria99@gmail.com>
2026-05-13 09:54:29 +0530

    practice qs

---
+++ Chapter-03/Practice Qs/qs_1.py
@@ -0,0 +1,21 @@
+# WAP to ask the user to enter name of their 3 favourite movies & store them in a list.
+
+# movies = []
+# mov1 = input("enter 1st movie:")
+# mov2 = input("enter 2nd movie:")
+# mov3 = input("enter 3rd movie:")
+
+# movies.append(mov1)
+# movies.append(mov2)
+# movies.append(mov3)
+
+# print(movies)
+
+# ---------------------------------------------------------------------------------------------------
+
+# movies = []
+# movies.append(input("enter 1st movie:"))
+# movies.append(input("enter 2nd movie:"))
+# movies.append(input("enter 3rd movie:"))
+
+# print(movies)
