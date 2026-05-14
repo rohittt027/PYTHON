2e6b3d730032197f8d48c70d5449029b83462b89
rohittt027 <rohitkaithwaria99@gmail.com>
2026-05-14 09:54:37 +0530

    learning update

--- Chapter-03/Practice Qs/qs_3.py
+++ Chapter-03/Practice Qs/qs_3.py
@@ -1,4 +1,4 @@
 # WAP to count the number of students with the "A" grade in the following tuple.
 
-grade = ("C", "D", "A", "A", "B", "B", "A")
+grade = ("C", "praD", "A", "A", "B", "B", "A")
 print(grade.count("A"))
