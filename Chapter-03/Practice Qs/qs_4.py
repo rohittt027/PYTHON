c7d5a474d0ff43bb5d0806c071bc821924f444c3
rohittt027 <rohitkaithwaria99@gmail.com>
2026-05-14 09:48:57 +0530

    practice qs

---
+++ Chapter-03/Practice Qs/qs_4.py
@@ -0,0 +1,5 @@
+# WAP to count the number of students with the "A" grade in the following tuple.("C", "D", "A", "A", "B", "B", "A") and Store the above values in a list & sort them from "A" to "D"
+
+grade = ["C", "D", "A", "A", "B", "B", "A"]
+grade.sort()
+print(grade)
